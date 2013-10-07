cda6f8d525b92d77a27f365c48c8456e0843dcb9
YM <stmishra@fastmail.fm>
2013-10-07 12:30:42 +0530

    Added dust, added a login function

--- app.js
+++ app.js
@@ -1,22 +1,67 @@
-var express = require ('express');
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-var app = express();
-app.get('/', function (req, res){
-	db.serialize(function() {
-		db.run('create table lorem (info TEXT)');
-		var stmt = db.prepare("INSERT into lorem VALUES (?)");
-		for (var i = 1; i < 10 ; i++){
-			stmt.run("Ipsum " + i);
-		}
-		stmt.finalize();
-		db.each("SELECT rowid as id, info from lorem", function (err, row) {
-				console.log(row.id + ":" + row.info);
-		});
-	});
-	db.close();
-	res.send("Hello, World!!");
-});
-app.listen(3000);
-console.log("App listening on port 3000");
+/* Various requires.
+ * express is our framework
+ */
 
+var express = require ( 'express' ),
+    fs      = require ( 'fs' ), //fs to access our sqlite file
+    file    = 'test.db',  //DB filename. Relative path.
+    exists  = fs.existsSync ( file ),//Check if the db already exists.
+    sqlite3 = require ( 'sqlite3' ).verbose ( ); //The node-sqlite3 library
+    db      = new sqlite3.Database ( file ), //The db handle
+    dust    = require ( 'dustjs-linkedin' ),//Dust, the templaing engine of choice.
+    cons    = require ( 'consolidate' ); //Consolidate, for dust to work with express.
+    store   = express.session.MemoryStore;
+
+
+/*
+ * Initialize the app 
+ */
+ 
+var app = express ( ) ;
+
+//Tell the app to use dust.js for templating
+app.engine ( 'dust' , cons.dust ) ;
+app.set ( 'template_engine', 'dust' ) ;
+
+
+//Other app settings
+app.use ( express.favicon() ) ; //Favicon
+app.use ( express.logger('dev' ) );//use logger in dev context
+app.use ( express.bodyParser ( ) );
+app.use ( express.methodOverride ( ) ) ;
+
+//We intend to use cookies and session for login.
+app.use ( express.cookieParser ( 'wigglybits' ) ) ;
+app.use ( express.session ( ) );
+
+/* Routes definitions */
+
+//Default route, e.g. localhost/
+app.get('/', function (req, res){
+
+   //Fetch from the database in a serial order.
+   // TODO : Figure out how to parallelize this.
+   
+   db.serialize(function() {
+       db.each("SELECT id ,title , post from entries ", function (err, row) {
+       console.log(row.id + ":" + row.title );
+	});
+   });
+   
+   db.close();
+   
+   res.send("Hello, World!!");
+   
+});
+
+app.get('/login', function (req, res){
+
+   res.send('Login page');
+   
+});
+
+
+app.listen(3000);
+
+//Tell the user the app has started
+console.log("App listening on port 3000");
