b27775323d0bb7fc0afeb19f92e02396fd847714
Santosh Mishra <stmishra@fastmail.fm>
2014-05-21 17:23:11 +0530

    Midori+pjax-bootstrap

--- static/js/site.js
+++ static/js/site.js
@@ -1,1 +1,19 @@
-$(document).pjax('a[data-pjax]', '#pjax-container',{localCache:true});
+midori.pjax = function(conf){
+
+    var pjax = new midoriAjax( function () {
+       midori.get(conf.container).innerHTML = pjax.responseText;
+    }, conf , conf.cache || false ),
+    pjaxUrl;
+
+    var headers = [['X-PJAX','true']];
+    //Finally, post.
+    pjax.post(conf.target, conf.data||'', conf.method || 'POST', headers );
+};
+
+routie('post/?:slug', function (slug){
+    midori.pjax({target: "/post/"+slug, container: "#pjax-container", cache: true, method: 'GET'} );
+});
+
+routie('', function(){
+ midori.pjax({target: "/" + models.post[0].href, container: "#pjax-container", cache: true, method:'GET'});
+});
